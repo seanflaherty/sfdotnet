6d23765e8821bac258b2c4fb0eb59cb0f7ad0bea
Sean Flaherty <sean.flaherty@gmail.com>
2014-12-07 15:23:30 -0500

    More scaffolding.

---
+++ templates/_footer.php
@@ -0,0 +1,22 @@
+  <footer class="footer_wrapper row">
+
+  </footer>
+
+  <script>
+  </script>
+    <script>
+    var GridlockBookmarkletConfig = {
+      useCookies: true,
+      position: "bottom-left"
+    };
+    (function() {
+      if (typeof GridlockBookmarklet == 'undefined') {
+        document.body.appendChild(document.createElement('script')).src = 'http://formstone.it/js/gridlock.bookmarklet.js';
+      } else {
+        GridlockBookmarklet();
+      }
+    })();
+  </script>
+</body>
+
+</html>
---
+++ templates/_header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <meta name="mobile-web-app-capable" content="yes">
+
+  <title>Sean Flaherty</title>
+  <!-- <script src="<?=$www_root?>components/jquery/dist/jquery.min.js"></script>
+  // <script src="<?=$www_root?>components/Boxer/jquery.fs.boxer.min.js"></script>
+  // <script src="js/main.js"></script> -->
+ <!-- <script scr="<?=$www_root?>components/modernizr/modernizr.js"></script>
+  <script src="js/site.min.js"></script>
+
+  <link rel="stylesheet" type="text/css" href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300|Roboto:400italic,700italic,700,400' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type='text/css' href="css/site.min.css"> -->
+  <!-- <link rel="stylesheet" href="css/site.min.css"> -->
+  <!-- <link rel="stylesheet" href="components/normalize-css/normalize.css">
+  <link rel="stylesheet" href="components/Gridlock/fs.gridlock.min.css">
+  <link rel="stylesheet" href="css/main.css"> -->
+
+</head>
+
+<body>
+  <header>
+  
+  </header>
