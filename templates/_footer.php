1c1a19c4f605005d780a2c9f20f5f6f8fc92daf3
Sean Flaherty <sean.flaherty@gmail.com>
2015-01-25 17:46:48 -0500

    Progress on the design.  Added social networking icons and resume placeholder.

--- templates/_footer.php
+++ templates/_footer.php
@@ -1,4 +1,15 @@
   <footer>
+  <div class="footer">
+      <div class="">
+          <a href="https://plus.google.com/+SeanFlaherty/posts" class="footer_list_item google">Google +</a>
+          <a href="https://twitter.com/sean_flaherty" class="footer_list_item twitter">Twitter</a>
+          <a href="www.linkedin.com/in/sflahe/" class="footer_list_item linkedin">Linked In</a>
+          <a href="https://github.com/seanflaherty" class="footer_list_item github">Github</a>
+      </div>
+      <div class="">
+          <p class="copyright">&copy; <?php echo date("Y"); ?> Sean Flaherty</p>
+      </div>
+  </div>
 
   </footer>
 
--- templates/home.php
+++ templates/home.php
@@ -2,18 +2,18 @@
     <div class="container">
         <nav class="navbar navbar-default" role="navigation">
             <div class="navbar-header">
-                <a class="" href="#">Home</a>
+                <a class="" href="<?=$www_root?>">Sean Flaherty</a>
             </div>
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <li>
-                        <a class="" href="#">Projects</a>
+                        <a class="" href="#projects">Projects</a>
                     </li>
                     <li>
-                        <a class="" href="#">Contact</a>
+                        <a class="" href="#contact">Contact</a>
                     </li>
                     <li>
-                        <a class="" href="#">Resume</a>
+                        <a class="" href="<?=$www_root?>assets/pdfs/Resume.pdf">Resume</a>
                     </li>
                     <li>
                         <a class="" href="http://seanflaherty.livejournal.com/">Blog</a>
@@ -21,5 +21,33 @@
                 </ul>
             </div>
         </nav>
+
+        <section class="intro">
+            <h2 class="intro-title tag">About Me</h2>
+            <p class="features-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit aliquet felis, quis.
+                <p>
+        </section>
+        <section class="projects">
+            <h2 class="projects-title tag">Projects</h2>
+            <a href= "images/photos/01.jpg" class ="project-image lightbox wow flipInX" data-gallery="gallery" title="BetaStore">
+                <img src="images/photos/thumb_01.jpg" alt="BetaStore">
+            </a>
+            <a href= "images/photos/01.jpg" class ="project-image lightbox wow flipInX" data-gallery="gallery" title="PicticiousPro">
+                <img src="images/photos/thumb_01.jpg" alt="PicticiousPro">
+            </a>
+            <a href= "images/photos/01.jpg" class ="project-image lightbox wow flipInX" data-gallery="gallery" title="Coffee Shop">
+                <img src="images/photos/thumb_01.jpg" alt="Coffee Shop">
+            </a>
+            <figure>
+                <img src="img_pulpit.jpg" alt="The Pulpit Rock">
+                <figcaption>A view of the pulpit rock in Norway.</figcaption>
+            </figure>
+        </section>
+        <section class="contact">
+            <h2 class="contact-title tag">Contact</h2>
+            <p class="features-text">Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum.
+                <p>
+        </section>
+
 
     </div>
